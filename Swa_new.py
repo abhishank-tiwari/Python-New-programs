f784a0f570f1ce8604ee286b83cc42886f325b57
abhishank-tiwari <qaer.abhi@gmail.com>
2023-06-15 20:21:56 -0700

    Swapping New technique

---
+++ Swa_new.py
@@ -0,0 +1,12 @@
+#Swapping variables
+a = 5
+b = 7
+
+print("Before swapping a ",a)
+print("Before swapping b",b)
+
+a,b = b,a
+
+print("\n")
+print("After swapping a ",a)
+print("After swapping b ",b)
