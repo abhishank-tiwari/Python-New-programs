0e0b181197b3e2d76cfb6d66b811f07c8612b628
abhishank-tiwari <qaer.abhi@gmail.com>
2023-06-21 18:25:14 -0700

    Sum of array

---
+++ Sum_of_arrays.py
@@ -0,0 +1,9 @@
+import numpy
+from numpy import *
+arr1 = array([1,2,3,4,5])
+arr2 = array([6,7,8,9,5])
+arr3 = arr1 + arr2
+
+print("First array is ",arr1)
+print("Second array is ",arr2)
+print("Sum of Both of them is ",arr3)
