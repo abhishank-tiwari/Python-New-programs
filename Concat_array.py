d022376cbe91963c2da999fce5c5eb3315d511cc
abhishank-tiwari <qaer.abhi@gmail.com>
2023-06-21 18:33:00 -0700

    Concatenate two array

---
+++ Concat_array.py
@@ -0,0 +1,8 @@
+from numpy import *
+arr1 = array([6,5,4,5,2])
+arr2 = array([7,6,3,4,8])
+arr3 = concatenate([arr1,arr2])
+
+print("First array is ",arr1)
+print("Second array is ",arr2)
+print("Concatenated array is ",arr3)
