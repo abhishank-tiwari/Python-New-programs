f2c90460d0e9136fa12728aeb10457fd9a391462
abhishank-tiwari <qaer.abhi@gmail.com>
2023-07-06 19:10:53 -0700

    Check character is digit or not

---
+++ isdigit_character.py
@@ -0,0 +1,9 @@
+#Check is a character is digit or not
+
+#Take input from user
+ch = input("Enter a character : ")
+#Check is a character is digit or not
+if(ch.isdigit()):
+    print("Given character ",ch," is a digit ")
+else:
+    print("Given character ",ch," is not a digit ")
